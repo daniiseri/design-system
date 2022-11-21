815c709fa0827a5e98c18ee925eae3df5735ad65
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-11-21 16:43:12 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello world</h1>
+
+      <button className="bg-cyan-500 font- px-4 hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  );
+}
+
+export { App };
